35682f3f8512310aad872b6603d0eaed93889c38
wabrara <65019679+wabrara@users.noreply.github.com>
2024-08-25 19:53:16 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 
+function animate() {
+  // 更新元素的位置
+  /*xPos += step;
+  element.style.transform = 'translateX(' + xPos + 'px)';
+
+  // 检查是否到达终点或需要继续动画
+  if (xPos < window.innerWidth) {
+    
+  }*/
+  var timeEl = document.getElementById("time");
+    timeEl.innerHTML = new Date();
+	console.log("更新内容：" + new Date().toLocaleTimeString());
+    requestAnimationFrame(animate);
+}
+
+// 开始动画
+requestAnimationFrame(animate);
